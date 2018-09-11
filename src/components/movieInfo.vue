b423d2da270883be2d70c7aba372dc1c6c1d9605
lucy <martian17218@gmail.com>
2018-09-11 13:45:01 +0800

    ✨ Feat: add overview in movieInfo

--- src/components/movieInfo.vue
+++ src/components/movieInfo.vue
@@ -3,27 +3,39 @@
     <Row style="width: 100%;">
       <Col span="12" offset="6">
         <VuePerfectScrollbar>
-        <section>
-          <header class="movie-details">
-            <span class="title">{{ movie.name }}</span>
-            <span class="date">{{ movie.release_date }}</span>
-          </header>
-          <div>
-            <p>片長： {{ movie.runtime }}{{ $t('minute') }}</p>
-            <p>導演： {{ movie.director }}</p>
-          </div>
-          <div style="margin: 20px 0;">
-            {{ movie.description }}
-          </div>
-        </section>
-        <section>
-          <aside class="float-left">
-            照片 評分 等等
-          </aside>
-          <div class="float-left" style="margin-left: 36px;">
-            演員 評論 討論 等等
-          </div>
-        </section>
+          <section>
+            <header class="movie-details">
+              <span class="title">{{ movie.name }}</span>
+              <span class="date">{{ movie.release_date }}</span>
+            </header>
+            <div>
+              <p>片長： {{ movie.runtime }}{{ $t('minute') }}</p>
+              <p>導演： {{ movie.director }}</p>
+            </div>
+            <div style="margin: 20px 0;">
+              {{ movie.description }}
+            </div>
+          </section>
+          <section class="flex">
+            <aside class="float-left fixed">
+              照片 評分 等等
+            </aside>
+            <div class="float-left stretch" style="margin-left: 36px;">
+              <div>
+                <Tabs>
+                  <TabPane label="演員"></TabPane>
+                  <TabPane label="劇組" disabled></TabPane>
+                  <TabPane label="類型" disabled></TabPane>
+                </Tabs>
+              </div>
+              <div>
+                <Tabs>
+                  <TabPane label="熱門評論"></TabPane>
+                  <TabPane label="近期評論"></TabPane>
+                </Tabs>
+              </div>
+            </div>
+          </section>
         </VuePerfectScrollbar>
       </Col>
     </Row>
@@ -43,10 +55,10 @@ export default {
       movie: {
         id: this.$route.query.mid,
         name: '--',
-        release_date: '--',
+        release_date: 'N/A',
         runtime: 0,
-        director: '--',
-        description: '--'
+        director: 'N/A',
+        description: 'N/A'
       }
     }
   },
@@ -60,6 +72,7 @@ export default {
 
         vm.movie.name = _data.title
         vm.movie.release_date = vm.$moment(_data.releaseDate).format('YYYY/MM')
+        vm.movie.description = _data.overview
       }
     )
   }
